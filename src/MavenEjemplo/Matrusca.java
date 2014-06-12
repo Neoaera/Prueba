f614cebb1fc382c584ce68c27652475d7a1cb917
Neoaera <aera_10@hotmail.com>
2014-06-12 19:28:14 -0400

    Ejercicio de las Matryoshkas V0.1 Valida los datos pero no hace ninguna operacion

---
+++ src/MavenEjemplo/Matrusca.java
@@ -0,0 +1,32 @@
+package MavenEjemplo;
+
+import javax.swing.JOptionPane;
+
+public class Matrusca {
+/**
+ * @author Andres Ramos
+ * @version 0,1
+ * 
+ */
+	public static void main(String[] args) {
+		try{
+		int numdemunh= Integer.parseInt(JOptionPane.showInputDialog("Cuantas Muñecas hay?"));
+		int tamdemun[]=new int[numdemunh];
+		for(int i=0;i<tamdemun.length;i++){
+			int j=i+1;
+			tamdemun[i]=Integer.parseInt(JOptionPane.showInputDialog("Ingrese tamaño de la Matryoshka numero "+j));
+			if(i!=0){
+				int k = i-1;
+				if(i==k){
+					System.out.println("IMPOSIBURUUUU!!");
+				}else{
+					System.out.println("Pato");
+				}
+			}
+		}
+		}catch(Exception e){
+			System.out.println("solamente tiene que ingresar numeros enteros");
+		}
+	}
+
+}
